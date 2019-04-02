d0e7d305a732e83e3ac3e046ba9e4c581aadf640
Guillaume Chapuis <guillaume.chapuis@stomshield.eu>
2019-04-02 15:59:46 +0200

    initial commit

---
+++ main.lisp
@@ -0,0 +1,53 @@
+; useless shit, to remove
+(defun factoriel (n)
+  (if (<= n 2)
+      n
+      (* n (factoriel (1- n)))))
+
+;;;;;;;;;;
+;; DATA ;;
+;;;;;;;;;;
+
+(defun make-empty-chess-board ()
+  (make-array '(8 8)))
+
+;;;;;;;;;;;;;
+;; GRAPHIC ;;
+;;;;;;;;;;;;;
+
+;; Helpers
+
+(defun repeat-character (n c)
+  (concatenate 'string (make-array n :initial-element c)))
+
+(defun interleave-char (line c)
+  (if (not line)
+      (list c)
+      (concatenate 'list
+                   (list c (car line))
+                   (interleave-char (cdr line) c))))
+
+(defun append-new-line (s)
+  (format t "~A~%" s #\return))
+
+;; Impl
+
+; taking an array from y level from chessboard
+(defun gen-separator ()
+  (repeat-character 10 #\-))
+
+(defun gen-middle-line (line annotation)
+  (concatenate 'string (cons annotation (interleave-char line #\|))))
+
+(defun generate-top-legends ()
+  (let ((ll (loop :for x :from 1 :to 8
+                  :collect (write-to-string x))))
+    (dolist (i ll)
+      (format t "~A " i))))
+
+; this will take 2d array (i.e a chess board) and aggregate it as
+; one string printable by the executable easily
+(defun print-chess-board (board)
+  (print "TODO"))
+
+(defun main () (print "TODO"))
